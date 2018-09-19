c85db14cc9cb2322d0dbbbef180ba2927614466b
slim <smedini@gmail.com>
2018-09-19 17:38:05 +0200

    draw v0.0.2 film v1.0.3

--- src/rest/filmRest.js
+++ src/rest/filmRest.js
@@ -6,8 +6,8 @@ import {deleteFilm, getFilm, getFilmByName, getFilms, saveFilm} from "../service
 const router = Router();
 
 router.get('/api/film/all', run(getFilms));
-router.get('/api/film/:_id', check("_id"), run(getFilm));
-router.get('/api/filmbyname/:name', check("name"), run(getFilmByName));
+router.get('/api/film/_id/:_id', check("_id"), run(getFilm));
+router.get('/api/film/name/:name', check("name"), run(getFilmByName));
 router.delete('/api/film/:_id', check("_id"), run(deleteFilm));
 router.post('/api/film', check("film"), run(saveFilm));
 

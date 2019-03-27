756c6271cf48ec4e66f5b0c639515899f8145006
slim <smedini@gmail.com>
2019-03-27 17:48:56 +0100

    2.0.3 - secure writes

--- src/rest/filmRest.js
+++ src/rest/filmRest.js
@@ -1,15 +1,16 @@
-import {Router} from "express-blueforest";
-import {check} from 'express-validator/check';
-import {run} from "express-blueforest";
-import {deleteFilm, getFilm, getFilmByName, getFilmRandom, getFilms, saveFilm} from "../service/filmService";
+import {Router} from "express-blueforest"
+import {check} from 'express-validator/check'
+import {run} from "express-blueforest"
+import {deleteFilm, getFilm, getFilmByName, getFilmRandom, getFilms, saveFilm} from "../service/filmService"
+import {validDrawer} from "../validations"
 
-const router = Router();
+const router = Router()
 
-router.get('/api/film/all', run(getFilms));
-router.get('/api/film/_id/:_id', check("_id"), run(getFilm));
-router.get('/api/film/random', run(getFilmRandom));
-router.get('/api/film/name/:name', check("name"), run(getFilmByName));
-router.delete('/api/film/:_id', check("_id"), run(deleteFilm));
-router.post('/api/film', check("film"), run(saveFilm));
+router.get('/api/film/all', run(getFilms))
+router.get('/api/film/_id/:_id', check("_id"), run(getFilm))
+router.get('/api/film/random', run(getFilmRandom))
+router.get('/api/film/name/:name', check("name"), run(getFilmByName))
+router.delete('/api/film/:_id', check("_id"), validDrawer, run(deleteFilm))
+router.post('/api/film', check("film"), validDrawer, run(saveFilm))
 
-module.exports = router;
+module.exports = router
